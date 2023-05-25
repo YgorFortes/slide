7296a42d59afd0d8368cb0df2e7569de4e9fc59d
Ygor Fortes <ygorsilva532@gmail.com>
2023-05-25 16:20:46 -0300

    configurações e index dos slides

--- js/slide.js
+++ js/slide.js
@@ -57,9 +57,45 @@ export default class Slide {
     this.ondEnd = this.ondEnd.bind(this);
   }
 
+  // Slides config
+
+  slidePosition (slide) {
+    const margin = (this.wrapper.offsetWidth - slide.offsetWidth) / 2;
+    return -(slide.offsetLeft - margin);
+  }
+
+  slidesConfig (){
+    this.slideArray = [...this.slide.children].map((element) => {
+      const position = this.slidePosition(element);
+      return {
+        position,
+        element
+      }
+    });
+  }
+
+  slidesIndexNav (index) {
+    const last = this.slideArray.length - 1;
+    this.index = {
+      prev: index ?  index - 1 : undefined,
+      active: index,
+      next: index === last ? undefined : index + 1,
+    }
+   
+  }
+
+  changeSlide (index) {
+    const activeSlide =  this.slideArray[index];
+    this.movoSlide(activeSlide.position);
+    this.slidesIndexNav(index);
+    this.dist.finalPosition = activeSlide.position;
+    console.log(this.index)
+  }
+
   init(){
     this.bindEvents();
     this.addSlideEvents();
+    this.slidesConfig();
     return this;
   }
 }
